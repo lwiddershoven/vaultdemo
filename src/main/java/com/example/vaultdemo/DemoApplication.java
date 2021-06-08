36f1a61df0c9a3db491773650a7992c65c950494
Leon Widdershoven <github@qaz.dds.nl>
2021-06-08 11:46:17 +0200

    Start status application

--- src/main/java/com/example/vaultdemo/DemoApplication.java
+++ src/main/java/com/example/vaultdemo/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "bcvaultexample");
+        System.setProperty("spring.profiles.active", "cbexample");
+//        System.setProperty("spring.profiles.active", "dbexample");
+//        System.setProperty("spring.profiles.active", "acexample");
+//        System.setProperty("spring.profiles.active", "bcvaultexample");
         SpringApplication.run(DemoApplication.class);
     }
 }
